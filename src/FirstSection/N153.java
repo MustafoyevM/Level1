e613d710f5c869604dec4c1e6e54da58e6a6d2da
MustafoyevM <88442272+MustafoyevM@users.noreply.github.com>
2022-06-05 12:46:13 +0500

    Examples

---
+++ src/FirstSection/N153.java
@@ -0,0 +1,11 @@
+package FirstSection;
+
+import java.util.Scanner;
+
+public class N153 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble(),b=sc.nextDouble();
+        System.out.println(a>0 && a<1 && b>0 && b<1);
+    }
+}
